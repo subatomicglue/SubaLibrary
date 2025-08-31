dc098096e7d722d152bd0f1f29594d90b59a38b6
km <bok@bok.com>
2025-08-31 09:23:56 -0500

    includes not include

--- serv/template.js
+++ serv/template.js
@@ -15,8 +15,8 @@ function data( d, vars = {} ) {
   });
   d = d.replace(/([\t ]*)<%include\s+(["'`])([^"'`]+)\2(?:\s+([a-zA-Z0-9_ ]+))?\s*%>/g, (match, whitespace, quotetype, filename, options) => {
     try {
-      options = options ? options.split(' ') : []; // check for options like: options.include( "force" ) 
-      let data2 = (options.include( "force" ) ? fs.readFileSync( filename ) : fs_readFileSync_cached(filename)).replace( /^/gm, whitespace )
+      options = options ? options.split(' ') : []; // check for options like: options.includes( "force" ) 
+      let data2 = (options.includes( "force" ) ? fs.readFileSync( filename ) : fs_readFileSync_cached(filename)).replace( /^/gm, whitespace )
       let value = data( data2, vars ); // recurse in case there's variables or other includes. 
       VERBOSE && logger.info(`[template] replacing: 'include ${filename}' -> '${value.replace(/^[\n\s]+/m, '').replace(/\n.*/gm, '' ).substring(0, 64)}'`);
       return value; // Read file contents
