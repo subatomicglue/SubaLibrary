789fcf890401b1172d298e69cf3a5a234094e747
km <bok@bok.com>
2025-09-11 12:54:18 -0500

    fix subtle template bug preventing edit and mangling some pages

--- serv/template.js
+++ serv/template.js
@@ -7,13 +7,17 @@ let indent = 0;
 
 function data( d, vars = {} ) {
   const VERBOSE = false;
+
+  if (d == undefined) return "";       // something went wrong, templates deal with strings, so return empty string.
+  if (typeof d !== "string") return d; // some data is numeric or object, we can't do template string replace with these types, just return them as is.
+
   d = d.replace(/<%=\s*([^\s%>]+)(?:\s+([a-zA-Z0-9_ ]+))?\s*%>/g, (match, key, options) => {
     options = options ? options.split(' ') : []; // check for options like: options.includes( "force" ) 
     let value = key in vars ? vars[key] : ((options.includes( "optional" )) ? '' : `!!! ${key} not set !!!`);
     indent++;
     value = data( value, vars ); // recurse in case there's variables or other includes. 
     indent--;
-    VERBOSE && console.log(' '.repeat(indent*2) + `[template] replacing: '${key}' -> '${value.replace(/\n/g, '\\n').substring(0, 32)}'`);
+    VERBOSE && console.log(' '.repeat(indent*2) + `[template] replacing: '${key}' -> '${JSON.stringify( value ).replace(/^"/,'').replace(/"$/,'').replace(/\n/g, '\\n').substring(0, 32)}'`);
     return value;
   });
   d = d.replace(/([\t ]*)<%include\s+(["'`])([^"'`]+)\2(?:\s+([a-zA-Z0-9_ ]+))?\s*%>/g, (match, whitespace, quotetype, filename, options) => {
@@ -23,7 +27,7 @@ function data( d, vars = {} ) {
       indent++;
       let value = data( data2, vars ); // recurse in case there's variables or other includes. 
       indent--;
-      VERBOSE && console.log(' '.repeat(indent*2) + `[template] replacing: [${whitespace}] 'include ${filename}' -> '${value.replace(/^[\n\s]+/m, '').replace(/\n.*/gm, '' ).substring(0, 64)}'`);
+      VERBOSE && console.log(' '.repeat(indent*2) + `[template] replacing: 'include ${filename} (ws:${whitespace.length})' -> '${JSON.stringify( value ).replace(/^"/,'').replace(/"$/,'').replace(/^[\n\s]+/m, '').replace(/\n.*/gm, '' ).substring(0, 64)}'`);
       return value; // Read file contents
     } catch (error) {
       console.log( `Error: Could not include "${filename}"`, error )
